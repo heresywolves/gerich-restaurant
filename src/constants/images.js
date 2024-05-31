c6bf26101a02b1ac22fa4c5d99b87d12eb144383
Your Namesifer <olegajun@gmail.com>
2024-05-31 21:29:33 +0300

    change all assetes to links to the assets on my server

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,47 +1,47 @@
 // import bg from '../assets/bg.webp';
 // import chef from '../assets/chef.webp';
-import G from '../assets/G.png';
+// import G from '../assets/G.png';
 // import gallery01 from '../assets/gallery01.webp';
 // import gallery02 from '../assets/gallery02.webp';
 // import gallery03 from '../assets/gallery03.webp';
 // import gallery04 from '../assets/gallery04.webp';
 // import knife from '../assets/knife.webp';
-import logo from '../assets/logo.png';
+// import logo from '../assets/logo.png';
 // import menu from '../assets/menu.webp';
 // import overlaybg from '../assets/overlaybg.webp';
-import spoon from '../assets/spoon.svg';
+// import spoon from '../assets/spoon.svg';
 // import welcome from '../assets/welcome.webp';
 // import findus from '../assets/findus.webp';
 // import laurels from '../assets/laurels.webp';
-import award01 from '../assets/award01.png';
-import award02 from '../assets/award02.png';
-import award03 from '../assets/award03.png';
-import award05 from '../assets/award05.png';
-import sign from '../assets/sign.png';
-import quote from '../assets/quote.png';
-import gericht from '../assets/gericht.png';
+// import award01 from '../assets/award01.png';
+// import award02 from '../assets/award02.png';
+// import award03 from '../assets/award03.png';
+// import award05 from '../assets/award05.png';
+// import sign from '../assets/sign.png';
+// import quote from '../assets/quote.png';
+// import gericht from '../assets/gericht.png';
 
 export default {
   bg: "https://user74.cloud-ru.vejio.su/uploads/files/634llo5j0qmk9sm4jl77ueku_bg.webp",
   chef: "https://user74.cloud-ru.vejio.su/uploads/files/634lma7y9bzvca39g7r9632u_chef.webp",
-  G,
+  G: "https://user74.cloud-ru.vejio.su/uploads/images/634m5s7v1o20i5zc321i9fna_g.png",
   gallery01: "https://user74.cloud-ru.vejio.su/uploads/files/634ln3xxtmo6bx06195m4k2f_gallery01.webp",
   gallery02: "https://user74.cloud-ru.vejio.su/uploads/files/634lna4r1aj7kzv4gkd4wz5c_gallery02.webp",
   gallery03: "https://user74.cloud-ru.vejio.su/uploads/files/634lnfl3c00y2yq3k472u62u_gallery03.webp",
   gallery04: "https://user74.cloud-ru.vejio.su/uploads/files/634lnp9ten24r3t6o678cx1b_gallery04.webp",
   knife: "https://user74.cloud-ru.vejio.su/uploads/files/634lo6u4a4y9917qwq2lh8ed_knife.webp",
-  logo,
+  logo: "https://user74.cloud-ru.vejio.su/uploads/images/634m6m79sv64577xrrpces72_logo.png",
   menu: "https://user74.cloud-ru.vejio.su/uploads/files/634m1o0v591uuz6g3bw5fjl6_menu.webp",
   overlaybg: "https://user74.cloud-ru.vejio.su/uploads/files/634m1o0veciui067jpd8vpgl_overlaybg.webp",
-  spoon,
+  spoon: "https://user74.cloud-ru.vejio.su/uploads/images/634m72gzzk8nj9zu4njh0wt7_spoon.png",
   welcome: "https://user74.cloud-ru.vejio.su/uploads/files/634m1o0vm999mhtadp81870r_welcome.webp",
   findus: "https://user74.cloud-ru.vejio.su/uploads/files/634m1o0sp1076i9054e58cb6_findus.webp",
   laurels: "https://user74.cloud-ru.vejio.su/uploads/files/634m1o0uwyvu7957ebltnq3t_laurels.webp",
-  award01,
-  award02,
-  award03,
-  award05,
-  sign,
-  quote,
-  gericht,
+  award01: "https://user74.cloud-ru.vejio.su/uploads/images/634m5s7nj83nk3x63hrj2d72_award01.png",
+  award02: "https://user74.cloud-ru.vejio.su/uploads/images/634m5s7pqxv952c3rnrolo6q_award02.png",
+  award03: "https://user74.cloud-ru.vejio.su/uploads/images/634m5s7q4wd5a8jj04l2vcn4_award03.png",
+  award05: "https://user74.cloud-ru.vejio.su/uploads/images/634m5s7qio193vj53u3dh3sa_award05.png",
+  sign: "https://user74.cloud-ru.vejio.su/uploads/images/634m6m7awl1r8mlbd3l54kjn_sign.png",
+  quote: "https://user74.cloud-ru.vejio.su/uploads/images/634m6m7a41al4z6ej7t1q4li_quote.png",
+  gericht: "https://user74.cloud-ru.vejio.su/uploads/images/634m6m77ax01vme3r2k127lf_gericht.png",
 };

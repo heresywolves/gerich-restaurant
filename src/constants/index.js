0c166dfb06a6dacf3ccb1524dd0ce0d13904290f
Your Namesifer <olegajun@gmail.com>
2024-05-31 22:09:32 +0300

    Revert image src to local assets

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 import images from './images';
-// import meal from '../assets/meal.webm';
+import meal from '../assets/meal.webm';
 import data from './data';
-const meal = "https://user74.cloud-ru.vejio.su/uploads/files/634m3bregch0n3de0r1l0400_meal.webm";
+// const meal = "https://user74.cloud-ru.vejio.su/uploads/files/634m3bregch0n3de0r1l0400_meal.webm";
 
 export { images, meal, data };
